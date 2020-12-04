ed045c5b37e8ed02c13ea3ad4eb12698183cdc14
Nhat Nguyen <nguyenhnhat@gmail.com>
2020-12-04 15:42:49 -0600

    first commit

--- src/index.js
+++ src/index.js
@@ -3,12 +3,60 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from 'redux';
+import allReducers from './reducers';
+import { Provider } from 'react-redux';
+// import {createStore} from 'redux';
+
+
+const store = createStore (
+  allReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+
+//STORE -> GLOBALIZED STATE
+
+//ACTION descriibes what you want to do action is INCREMENT. A function a object
+// const increment = () => {
+//   return {
+//     type: 'INCREMENT'
+//   }
+// }
+
+// const decrement = () => {
+//   return {
+//     type: 'DECREMENT'
+//   }
+// }
+
+
+//REDUCER descirbe how your action transform state to next state . reducer will check what action you did then motify the store
+
+// const counter = (state = 0, action) => {
+//   switch(action.type) {
+//     case 'INCREMENT':
+//       return state + 1;
+//     case 'DECREMENT':
+//       return state - 1; 
+//   }
+// }
+
+// let store = createStore(counter);
+
+//display it in the console
+// store.subscribe(() => console.log(store.getState()));
+
+//DISPATCH excute the action
+// store.dispatch(increment());
+// store.dispatch(decrement());
+// store.dispatch(decrement());
 
 ReactDOM.render(
-  <React.StrictMode>
+ <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    </Provider>
+  ,document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
